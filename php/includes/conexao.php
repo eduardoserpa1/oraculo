eccd34c4dc4d88190e5802c29ae02bce55085328
BuildTools <unconfigured@null.spigotmc.org>
2019-12-02 20:04:32 -0300

    passo1

---
+++ php/includes/conexao.php
@@ -0,0 +1,9 @@
+<?php
+
+ $link = mysqli_connect('173.199.117.116', 'asso2019_iann', 'abc123', 'asso2019_iann');
+    mysqli_query($link, "SET NAMES 'utf8'");
+    mysqli_query($link, 'SET character_set_connection=utf8');
+    mysqli_query($link, 'SET character_set_client=utf8');
+    mysqli_query($link, 'SET character_set_results=utf8');
+
+?>
