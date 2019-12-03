f3a04b6b97947fa4dc1f95222c12aee299cab0bd
BuildTools <unconfigured@null.spigotmc.org>
2019-12-03 01:52:57 -0300

    set receitas

--- funcoes/funcaoReceitas.js
+++ funcoes/funcaoReceitas.js
@@ -1,29 +1,61 @@
 var listaProdutos = [];//lista de atalhos existentes
 
-//cria a lista de atalhos
-function criaListaAtalhos(nome_produto,id_produto){
+//cria a lista de produtos
+function criaListaProdutos(nome_produto,id_produto){
   listaProdutos.push(id_produto,nome_produto);
+  listaListaProdutos();
+}
 
+//lista a lista de produtos
+function listaListaProdutos(){
   $(function(){
     $('#listaDeProdutos').empty();
     for(i=0;i<listaProdutos.length;i+=2){
       //alert(listaProdutos[i]+listaProdutos[i+1]);
-      $('#listaDeProdutos').append('<p href="#" class="w3-bar-item w3-button w3-padding" style="border-radius: 10px; background-color: rgb(173,172,172);"onclick="">'+listaProdutos[i]+'<input class="w3-right" type="button" value="Delete"></p>'); 
+      $('#listaDeProdutos').append('<p href="#" class="w3-bar-item w3-button w3-padding" style="border-radius: 10px; background-color: rgb(173,172,172);"onclick="">'+listaProdutos[i]+'<input class="w3-right" onclick="deletLinhaProduto('+i+')" type="button" value="Delete"></p>'); 
     }
   });
 
-  //atualizaListaAtalhos();
+
 }
-/*
-{
-  $('#listaDeAtalhos').append('<p href="#" class="w3-bar-item w3-button w3-padding" style="border-radius: 10px; background-color: rgb(173,172,172);"onclick="document.getElementById('+listaAtalhos[i][0][1]+').play();">'+String.fromCharCode(listaAtalhos[i][0][0])+' = '+listaAtalhos[i][0][2]+'<i class="w3-right w3-large fas fa-times-circle" onclick="alert('+lari+');"></i><i class="w3-right w3-large fas fa-play"></i></p>'); 
-}*/
+
+//deleta os produtos
+function deletLinhaProduto(i){
+  listaProdutos.splice(i,2);
+  listaListaProdutos();
+
+}
+
 
 //seleciona o audio para criar o atalho
 function selctProduto(id_produto,nome_produto){
-    criaListaAtalhos(id_produto,nome_produto);//atualiza a lista de atalhos
+    criaListaProdutos(id_produto,nome_produto);//atualiza a lista de atalhos
     console.log(document.cookie);
 }
+
+//seta a relação receita produto
+function receitaProduto(){
+  valores="";
+  for(i=0;i<listaProdutos.length;i+=2){
+    aux = listaProdutos[i+1];
+    if(i==0){
+      valores+=aux;
+    }else{
+      valores+=','+aux;
+    }
+  }
+  $.ajax({
+    url: 'http://localhost/oraculo-1/php/requests/setReceita&Produtos.php?valores=' + valores,
+    method: 'POST',
+    contentType: 'application/json',
+    success: function(response) {
+      console.log(response);
+      $('#get-button').click();
+      }
+  });
+}
+
+
 
 
 
