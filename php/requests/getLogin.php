eb5bc7654b0b42c5df777caceb1c449d48608891
eduardoserpa1 <47951275+eduardoserpa1@users.noreply.github.com>
2019-12-02 22:44:33 -0300

    Add files via upload

---
+++ php/requests/getLogin.php
@@ -0,0 +1,72 @@
+<?php
+
+include '../includes/conexao.php';
+
+if (!empty($_POST['enviar_login'])) {
+	
+
+	$identifica='null';
+
+	$email = addslashes($_POST['email']);
+	$senha = addslashes(md5($_POST['senha']));
+
+	if (!empty($email) && !empty($senha)) {
+	
+		$query = "SELECT * FROM Consumidor WHERE email_consumidor like '$email' 
+											and senha_consumidor like '$senha' ";
+		
+		$resultado = mysqli_query($link,$query);
+
+		$num_row = mysqli_affected_rows($link);
+
+		if ($num_row>0) {
+			$identifica = "consumidor";
+
+		}
+
+
+		$query = "SELECT * FROM Fornecedor WHERE email_fornecedor like '$email' 
+											and senha_fornecedor like '$senha' ";
+		
+		$resultado = mysqli_query($link,$query);
+
+		$num_row = mysqli_affected_rows($link);
+
+		if ($num_row>0) {
+			$identifica = "fornecedor";
+		}
+
+
+		if ($identifica == "fornecedor") {
+			$query = "SELECT * FROM Fornecedor WHERE email_fornecedor like '$email' 
+											and senha_fornecedor like '$senha' ";
+			$resultado = mysqli_query($link,$query);
+
+			$linha = mysqli_fetch_array($resultado);
+
+			$_SESSION['id_sessao'] = $linha['id_fornecedor'];
+
+			echo "o usuario eh um fornecedor";
+		}
+
+		if ($identifica == "consumidor") {
+			$query = "SELECT * FROM Consumidor WHERE email_consumidor like '$email' 
+											and senha_consumidor like '$senha' ";
+			$resultado = mysqli_query($link,$query);
+
+			$linha = mysqli_fetch_array($resultado);
+
+			$_SESSION['id_sessao'] = $linha['id_consumidor'];
+
+			echo "o usuario eh um consumidor";
+		}
+
+		if ($identifica == "null") {
+			echo "usuario ou senha nao encontrados";
+		}
+
+
+	}
+}
+
+?>
--- php/requests/setFornecedor.php
+++ php/requests/setFornecedor.php
@@ -5,25 +5,34 @@ include '../includes/conexao.php';
 if (!empty($_POST['enviar_cadastro_fornecedor'])) {
 	
 	$nome = addslashes($_POST['nome']);
-	$email = addslashes($_POST['nome']);
-	$senha = addslashes(md5($_POST['nome']));
-	$endereco = addslashes($_POST['nome']);
-	$telefone = addslashes($_POST['nome']);
+	$email = addslashes($_POST['email']);
+	$senha = addslashes(md5($_POST['senha']));
+	$endereco = addslashes($_POST['endereco']);
+	$telefone = addslashes($_POST['telefone']);
 
 
 
 
-	if (!empty($nome) && !empty($email) && !empty($senha) && !empty($endereco) && !empty($telefone)) {
 	
-		$query = "INSERT INTO Consumidor(	nome_fornecedor,
-											email_fornecedor,
-											senha_fornecedor,
-											endereco_fornecedor,
-											telefone_fornecedor)
-					VALUES('$nome','$email','$senha','$endereco','$telefone')";
-		mysqli_query($link,$query);
 
-	}
+		$query = "SELECT * FROM Consumidor WHERE email_consumidor like '$email' ";
+		
+		$resultado = mysqli_query($link,$query);
+
+		$num_row = mysqli_affected_rows($link);
+
+		if ($num_row==0) {
+			
+			$query = "INSERT INTO Fornecedor(	nome_fornecedor,
+												email_fornecedor,
+												senha_fornecedor,
+												endereco_fornecedor,
+												telefone_fornecedor)
+												
+						VALUES('$nome','$email','$senha','$endereco','$telefone')";
+			mysqli_query($link,$query);
+		}
+	
 }
 
 ?>
