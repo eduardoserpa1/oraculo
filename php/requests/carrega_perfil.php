616ed3721c16dbafe6017b1f74dbfe1f3a428960
BuildTools <unconfigured@null.spigotmc.org>
2019-12-13 09:28:51 -0300

    deletei o carrega perfil

--- php/requests/carrega_perfil.php
+++ php/requests/carrega_perfil.php
@@ -1,29 +1,5 @@
 <?php
 
-session_start();
-
-include '../includes/conexao.php';
-
-$id = $_SESSION['id_sessao'];	
-
-	$query = "SELECT * FROM Fornecedor WHERE id_fornecedor = $id ";
-
-	$resultado = mysqli_query($link,$query);
-
-	if( $linha = mysqli_fetch_array($resultado) ){
-
-	$s = "";
-
-	$s .= $linha['nome_fornecedor'].",";
-	$s .= $linha['telefone_fornecedor'].",";
-	$s .= $linha['endereco_fornecedor'];
-
-
-	echo $s;
-	
-	}
-
-
 
 ?>
 
